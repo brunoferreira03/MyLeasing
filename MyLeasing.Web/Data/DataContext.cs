c18fbb2d115bbf4f0f667fb05522da2d05b4a897
bf03 <bff3rreira03@gmail.com>
2023-05-31 15:52:37 +0100

    CRUD Owners

---
+++ MyLeasing.Web/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGeneration.EntityFrameworkCore;
+using MyLeasing.Web.Data.Entity;
+
+namespace MyLeasing.Web.Data
+{ 
+    public class DataContext : DbContext
+    {
+        public DbSet<Owner> Owners { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ MyLeasing.Web/Data/Entity/Owner.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyLeasing.Web.Data.Entity
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Document { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [MaxLength(9)]
+        [Display(Name = "Home Phone")]
+        public string Fixed_Phone { get; set; }
+
+        [Required]
+        [MaxLength(9)]
+        [Display(Name = "Cell Phone")]
+        public string Cell_Phone { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+    }
+}
